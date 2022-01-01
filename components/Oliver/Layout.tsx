f85ecb699dc2ccc601997e03ea7247f95d6bdd9c
oliverlevay <oliver.levay@hotmail.com>
2022-01-01 18:56:20 +0100

    Add some meta tags to olivers page

--- components/Oliver/Layout.tsx
+++ components/Oliver/Layout.tsx
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import Layout from "components/Layout";
-import { Typography, Stack, Tabs, Tab } from "@mui/material";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Social from "./Social";
+import Head from 'next/head';
+import Layout from 'components/Layout';
+import { Typography, Stack, Tabs, Tab } from '@mui/material';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Social from './Social';
 
 const OliverLayout = ({
   children,
@@ -14,7 +14,7 @@ const OliverLayout = ({
   const [activeTab, setActiveTab] = useState(0);
   const router = useRouter();
   useEffect(() => {
-    if (router.asPath === "/oliver") {
+    if (router.asPath === '/oliver') {
       setActiveTab(0);
     } else {
       setActiveTab(1);
@@ -22,16 +22,27 @@ const OliverLayout = ({
   }, []);
   useEffect(() => {
     if (activeTab === 0) {
-      router.push("/oliver");
+      router.push('/oliver');
     } else if (activeTab === 1) {
-      router.push("/oliver/projects");
+      router.push('/oliver/projects');
     }
   }, [activeTab]);
 
   return (
     <Layout title="Oliver">
       <Head>
-        <title>Oliver | Sakur</title>
+        <title>Oliver at Sakur</title>
+        <meta property="og:title" content="Oliver at Sakur" />
+        <meta property="og:url" content="https://www.sakur.se/oliver" />
+        <meta
+          property="og:description"
+          content="My name is Oliver Levay and I like developing websites. Here you can read more about me."
+        />
+        <meta
+          property="og:image"
+          content="https://www.sakur.se/images/oliver-slogan.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Stack spacing={1}>
         <Typography variant="h2">Fullstack Developer</Typography>
